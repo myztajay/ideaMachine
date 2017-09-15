23b291442442d9aa37702879ef7f2c22ada3a159
myztajay <myztajay@hotmail.com>
2017-09-15 16:16:32 -0400

    Added Router and routes - Created Home & Projects components

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Home from './components/Home'
+import Projects from './components/Projects'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+    
+      <BrowserRouter>
+        <div>
+          <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/projects" component={Projects} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Projects.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Projects extends Component{
+  render(){
+    return(
+      <h1> These are my Projects</h1>
+    )
+  }
+}
+
+export default Projects;
