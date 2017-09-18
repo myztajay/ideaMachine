f92bf53e9fb96a82784e98905dfcecf989ad2049
myztajay <myztajay@hotmail.com>
2017-09-18 10:32:18 -0400

    Added css to fix cards displays and got temp images up

--- src/common/ProjectCard.js
+++ src/common/ProjectCard.js
@@ -4,7 +4,7 @@ import { Card, CardTitle } from 'react-materialize';
 const ProjectCard = ({title, description, technologies}) =>{
   return(
     <Card className='small'
-      header={<CardTitle image='./compare2.jpg'>{title}</CardTitle>}
+      header={<CardTitle image='images/compare2.jpg'>{title}</CardTitle>}
       actions={[<a href='#'>This is a Link</a>]}>
       {description}
     </Card>
